babd29e3be1f69e3f3494c24b1dce7b009665f45
Vincent B. Abolarin <vincentab@ha-shem.com>
2023-01-25 12:20:39 +0100

    build Login page UI with mobile responsiveness

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "../styles/home.module.scss"
 
 import LendsqrLogoIcon from "../../src/assets/lendsqrLogoIcon.svg";
 
 const Home = () => {
     return (
-        <div>
-            <div className="App-header">
-                <img src={LendsqrLogoIcon} className="App-logo" alt="logo" />
+        <div className={`container ${styles.container}`}>
+            <div className={styles.logoContainer}>
+                <img src={LendsqrLogoIcon} className={styles.logo} alt="logo" />
             </div>
             <Link to="login">
                 <button>Login</button>
