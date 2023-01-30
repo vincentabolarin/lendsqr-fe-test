dcbcbb6cac03c3eebad7e30d338c2c42cd2b62fb
Vincent B. Abolarin <vincentab@ha-shem.com>
2023-01-30 23:16:28 +0100

    initialize sidebar setup

---
+++ src/constants/sidebarItems.ts
@@ -0,0 +1,40 @@
+import Users from "src/assets/users.svg";
+import Guarantors from "assets/guarantors.svg";
+import Loans from "..//assets/loans.svg";
+
+const customersMenu = [
+  {
+    icon: "users",
+    text: "Users",
+  },
+  {
+    icon: "guarantors",
+    text: "Guarantors",
+  },
+  {
+    icon: "loans",
+    text: "Loans",
+  },
+];
+const businessesMenu = [
+    {
+        icon: "icon",
+        text: "Dashboard"
+    },
+    {
+        icon: "icon3",
+        text: "newText"
+    }
+]
+const settingsMenu = [
+    {
+        icon: "icon",
+        text: "Dashboard"
+    },
+    {
+        icon: "icon3",
+        text: "newText"
+    }
+]
+
+export { customersMenu, businessesMenu, settingsMenu };
