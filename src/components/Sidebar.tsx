18b86fb390a3a3cbfe33143b122272e94f2bff9b
Vincent B. Abolarin <vincentab@ha-shem.com>
2023-01-31 01:02:59 +0100

    work on sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,24 +3,67 @@ import styles from "../styles/sidebar.module.scss";
 
 import { customersMenu, businessesMenu, settingsMenu } from "../constants/sidebarItems.ts";
 
+import SwitchOrganization from "../assets/switchOrganization.svg";
+import Dropdown from "../assets/dropdown.svg";
+import Dashboard from "../assets/dashboard.svg";
+
 const Sidebar = (props) => {
     const { children } = props;
     return (
-        <div className={`${styles.container} `}>
-            <div className={styles.sidebarContainer}>
-                {
-                    customersMenu.map((customersMenu) => {
-                        return (
-                          <div>
-                            <img src={require("../assets/" + customersMenu.icon + ".svg")} alt={customersMenu.text} />
-                            <h3>{ customersMenu.text }</h3>
-                          </div>
-                        );
-                    })
-                }
-            </div>
-            {children}
+      <div className={`${styles.container} `}>
+        <div className={styles.sidebarContainer}>
+          <div className={`${styles.sidebarItem} ${styles.switchOrganization}`}>
+            <img src={SwitchOrganization} alt="Switch Organization" />
+            <p>Switch Organization</p>
+            <img src={Dropdown} alt="Dropdown" />
+          </div>
+
+          <div className={`${styles.sidebarItem} ${styles.dashboard}`}>
+            <img src={Dashboard} alt="Dashboard" />
+            <p>Dashboard</p>
+          </div>
+
+          <p className={styles.heading}>CUSTOMERS</p>
+          {customersMenu.map((customersMenu, index) => {
+            return (
+              <div className={styles.sidebarItem} key={index}>
+                <img
+                  src={require("../assets/" + customersMenu.icon + ".svg")}
+                  alt={customersMenu.text}
+                />
+                <p>{customersMenu.text}</p>
+              </div>
+            );
+          })}
+
+          <p className={styles.heading}>BUSINESSES</p>
+          {businessesMenu.map((businessesMenu, index) => {
+            return (
+              <div className={styles.sidebarItem} key={index}>
+                <img
+                  src={require("../assets/" + businessesMenu.icon + ".svg")}
+                  alt={businessesMenu.text}
+                />
+                <p>{businessesMenu.text}</p>
+              </div>
+            );
+          })}
+
+          <p className={styles.heading}>SETTINGS</p>
+          {settingsMenu.map((settingsMenu, index) => {
+            return (
+              <div className={styles.sidebarItem} key={index}>
+                <img
+                  src={require("../assets/" + settingsMenu.icon + ".svg")}
+                  alt={settingsMenu.text}
+                />
+                <p>{settingsMenu.text}</p>
+              </div>
+            );
+          })}
         </div>
+        {children}
+      </div>
     );
 }
  
